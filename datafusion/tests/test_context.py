96ac51c9bb62dc6594c8dd99fc5feeb946913ff9
Eero <eero.lihavainen@gmail.com>
2022-07-29 08:00:01 +0300

    more formatting

--- datafusion/tests/test_context.py
+++ datafusion/tests/test_context.py
@@ -21,21 +21,23 @@ import pyarrow.dataset as ds
 from datafusion import column, literal, SessionContext
 import pytest
 
+
 def test_create_context_no_args():
     SessionContext()
 
+
 def test_create_context_with_all_valid_args():
     ctx = SessionContext(
-            target_partitions=1,
-            default_catalog="foo",
-            default_schema="bar",
-            create_default_catalog_and_schema=True,
-            information_schema=True,
-            repartition_joins=False,
-            repartition_aggregations=False,
-            repartition_windows=False,
-            parquet_pruning=False
-            )
+        target_partitions=1,
+        default_catalog="foo",
+        default_schema="bar",
+        create_default_catalog_and_schema=True,
+        information_schema=True,
+        repartition_joins=False,
+        repartition_aggregations=False,
+        repartition_windows=False,
+        parquet_pruning=False,
+    )
 
     # verify that at least some of the arguments worked
     ctx.catalog("foo").database("bar")
