cb32abfe9a5cacc6e8a3c652747f08e9f7c58ae1
Andy Grove <andygrove73@gmail.com>
2022-11-18 07:31:38 -0700

    fix?

--- datafusion/tests/test_context.py
+++ datafusion/tests/test_context.py
@@ -39,7 +39,7 @@ def test_create_context_with_all_valid_args():
         repartition_aggregations=False,
         repartition_windows=False,
         parquet_pruning=False,
-        config_options = { "foo": "bar" }
+        config_options=None,
     )
 
     # verify that at least some of the arguments worked
