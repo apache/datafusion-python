112df24f93dc44518fda7d8dc1fdbba1ef7a842c
Jeremy Dyer <jdye64@gmail.com>
2023-02-06 19:58:16 -0500

    format!() changes

--- src/sql/exceptions.rs
+++ src/sql/exceptions.rs
@@ -25,18 +25,18 @@ create_exception!(rust, ParsingException, pyo3::exceptions::PyException);
 // Identifies exceptions that occur during attempts to optimization an existing `LogicalPlan`
 create_exception!(rust, OptimizationException, pyo3::exceptions::PyException);
 
-pub fn py_type_err(e: impl Debug) -> PyErr {
-    PyErr::new::<pyo3::exceptions::PyTypeError, _>(format!("{:?}", e))
+pub fn py_type_err(e: impl Debug + std::fmt::Display) -> PyErr {
+    PyErr::new::<pyo3::exceptions::PyTypeError, _>(format!("{e}"))
 }
 
-pub fn py_runtime_err(e: impl Debug) -> PyErr {
-    PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!("{:?}", e))
+pub fn py_runtime_err(e: impl Debug + std::fmt::Display) -> PyErr {
+    PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!("{e}"))
 }
 
-pub fn py_parsing_exp(e: impl Debug) -> PyErr {
-    PyErr::new::<ParsingException, _>(format!("{:?}", e))
+pub fn py_parsing_exp(e: impl Debug + std::fmt::Display) -> PyErr {
+    PyErr::new::<ParsingException, _>(format!("{e}"))
 }
 
-pub fn py_optimization_exp(e: impl Debug) -> PyErr {
-    PyErr::new::<OptimizationException, _>(format!("{:?}", e))
+pub fn py_optimization_exp(e: impl Debug + std::fmt::Display) -> PyErr {
+    PyErr::new::<OptimizationException, _>(format!("{e}"))
 }
