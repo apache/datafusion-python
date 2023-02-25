d1f656726042252b0b948e119c664df04468f19b
Dejan Simic <10134699+simicd@users.noreply.github.com>
2023-02-25 20:57:46 +0100

    feature: Additional export methods (#236)
    * Implement to_arrow_table, to_pylist and to_pydict
    
    * Write unit tests for empty dataframes
    
    * Document examples
    
    * Fix formatting
    
    * Remove unused import
    
    * Implement to_polars() method
    
    * Remove unused import

---
+++ examples/export.py
@@ -0,0 +1,58 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import datafusion
+import pyarrow
+
+
+# create a context
+ctx = datafusion.SessionContext()
+
+# create a RecordBatch and a new datafusion DataFrame from it
+batch = pyarrow.RecordBatch.from_arrays(
+    [pyarrow.array([1, 2, 3]), pyarrow.array([4, 5, 6])],
+    names=["a", "b"],
+)
+df = ctx.create_dataframe([[batch]])
+# Dataframe:
+# +---+---+
+# | a | b |
+# +---+---+
+# | 1 | 4 |
+# | 2 | 5 |
+# | 3 | 6 |
+# +---+---+
+
+# export to pandas dataframe
+pandas_df = df.to_pandas()
+assert pandas_df.shape == (3, 2)
+
+# export to PyArrow table
+arrow_table = df.to_arrow_table()
+assert arrow_table.shape == (3, 2)
+
+# export to Polars dataframe
+polars_df = df.to_polars()
+assert polars_df.shape == (3, 2)
+
+# export to Python list of rows
+pylist = df.to_pylist()
+assert pylist == [{"a": 1, "b": 4}, {"a": 2, "b": 5}, {"a": 3, "b": 6}]
+
+# export to Pyton dictionary of columns
+pydict = df.to_pydict()
+assert pydict == {"a": [1, 2, 3], "b": [4, 5, 6]}
