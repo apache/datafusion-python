0f2cd2e1be1406f0a462f2b106c41a111f90f37c
Jeremy Dyer <jdye64@gmail.com>
2023-05-04 20:54:10 -0400

    Add  'pub' and '#[pyo3(get, set)]' to DataTypeMap (#371)

--- src/common/data_type.rs
+++ src/common/data_type.rs
@@ -33,12 +33,12 @@ use crate::errors::py_datafusion_err;
 #[derive(Debug, Clone)]
 #[pyclass(name = "DataTypeMap", module = "datafusion.common", subclass)]
 pub struct DataTypeMap {
-    #[allow(dead_code)]
-    arrow_type: PyDataType,
-    #[allow(dead_code)]
-    python_type: PythonType,
-    #[allow(dead_code)]
-    sql_type: SqlType,
+    #[pyo3(get, set)]
+    pub arrow_type: PyDataType,
+    #[pyo3(get, set)]
+    pub python_type: PythonType,
+    #[pyo3(get, set)]
+    pub sql_type: SqlType,
 }
 
 impl DataTypeMap {
@@ -421,7 +421,7 @@ impl DataTypeMap {
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 #[pyclass(name = "DataType", module = "datafusion.common")]
 pub struct PyDataType {
-    data_type: DataType,
+    pub data_type: DataType,
 }
 
 impl From<PyDataType> for DataType {
