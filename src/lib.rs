da6c183ebb673b27808ff35b1b9fd8d577a203c0
Jeremy Dyer <jdye64@gmail.com>
2023-10-26 14:24:43 -0400

    Remove libprotobuf dep (#527)
    * Remove libprotobuf as a build time dependency of Conda
    
    * Add substrait feature which enables building with substrait features
    
    * Add datafusion._internal/substrait to the pyproject.toml file so substrait will be built for pytests when using pip
    
    * Rename feature to just substrait
    
    * Add protoc since libprotobuf was removed from conda requirements file
    
    * Refactor substrait module being initialized as a function
    
    * Reintroduce libprotobuf as build dependency for conda build

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,8 @@ pub use datafusion_common;
 pub use datafusion_expr;
 pub use datafusion_optimizer;
 pub use datafusion_sql;
+
+#[cfg(feature = "substrait")]
 pub use datafusion_substrait;
 
 #[allow(clippy::borrow_deref_ref)]
@@ -48,6 +50,8 @@ mod pyarrow_filter_expression;
 mod record_batch;
 pub mod sql;
 pub mod store;
+
+#[cfg(feature = "substrait")]
 pub mod substrait;
 #[allow(clippy::borrow_deref_ref)]
 mod udaf;
@@ -108,9 +112,16 @@ fn _internal(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_submodule(store)?;
 
     // Register substrait as a submodule
+    #[cfg(feature = "substrait")]
+    setup_substrait_module(py, m)?;
+
+    Ok(())
+}
+
+#[cfg(feature = "substrait")]
+fn setup_substrait_module(py: Python, m: &PyModule) -> PyResult<()> {
     let substrait = PyModule::new(py, "substrait")?;
     substrait::init_module(substrait)?;
     m.add_submodule(substrait)?;
-
     Ok(())
 }
