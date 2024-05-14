e5d577fdfa2f5a09080d12b2197cb6901b35f6c8
Richard Tia <richie.tia@gmail.com>
2024-05-14 11:50:48 -0400

    refactor: update arg names

--- src/functions.rs
+++ src/functions.rs
@@ -407,6 +407,7 @@ expr_fn!(cosh, num);
 expr_fn!(degrees, num);
 expr_fn!(decode, input encoding);
 expr_fn!(encode, input encoding);
+expr_fn!(ends_with, string suffix, "Returns true if string ends with suffix.");
 expr_fn!(exp, num);
 expr_fn!(factorial, num);
 expr_fn!(floor, num);
@@ -473,8 +474,7 @@ expr_fn!(
     "Splits string at occurrences of delimiter and returns the n'th field (counting from one)."
 );
 expr_fn!(sqrt, num);
-expr_fn!(starts_with, arg1 arg2, "Returns true if string starts with prefix.");
-expr_fn!(ends_with, arg1 arg2, "Returns true if string ends with suffix.");
+expr_fn!(starts_with, string prefix, "Returns true if string starts with prefix.");
 expr_fn!(strpos, string substring, "Returns starting index of specified substring within string, or zero if it's not present. (Same as position(substring in string), but note the reversed argument order.)");
 expr_fn!(substr, string position);
 expr_fn!(tan, num);
