7b66cea0877e38075bf481c1a202c1d32d639fc3
Tim Saucer <timsaucer@gmail.com>
2024-05-18 10:55:18 -0400

    Parameterize the tpch testing

--- examples/tpch/_tests.py
+++ examples/tpch/_tests.py
@@ -1,107 +1,95 @@
-import os
+import pytest
+from importlib import import_module
 import pyarrow as pa
 from datafusion import col, lit, functions as F, Expr
 
-os.environ["TPCH_USE_REF_DATA"] = "TRUE"
+def df_selection(col_name, col_type):
+    if col_type == pa.float64() or isinstance(col_type, pa.Decimal128Type):
+        return F.round(col(col_name), lit(2)).alias(col_name)
+    elif col_type == pa.string():
+        return F.trim(col(col_name)).alias(col_name)
+    else:
+        return col(col_name)
 
-def compare_decimal(value, expected) -> bool:
-    return abs(value.cast(pa.float64()).as_py() - expected) < 0.01
+def load_schema(col_name, col_type):
+    if col_type == pa.int64() or col_type == pa.int32():
+        return col_name, pa.string()
+    elif isinstance(col_type, pa.Decimal128Type):
+        return col_name, pa.float64()
+    else:
+        return col_name, col_type
+    
+def expected_selection(col_name, col_type):
+    if col_type == pa.int64() or col_type == pa.int32():
+        return F.trim(col(col_name)).cast(col_type).alias(col_name)
+    elif col_type == pa.string():
+        return F.trim(col(col_name)).alias(col_name)
+    else:
+        return col(col_name)
 
-def round_decimal(col_name: str) -> Expr:
-    return F.round(col(col_name), lit(2)).alias(col_name)
+def selections_and_schema(original_schema):
+    columns = [ (c, original_schema.field(c).type) for c in original_schema.names ]
 
-def test_q01_pricing_summary_report():
-    import q01_pricing_summary_report
-    df = q01_pricing_summary_report.df
+    df_selections = [ df_selection(c, t) for (c, t) in columns]
+    expected_schema = [ load_schema(c, t) for (c, t) in columns]
+    expected_selections = [ expected_selection(c, t) for (c, t) in columns]
 
-    df = df.select(
-        col("l_returnflag"),
-        col("l_linestatus"),
-        round_decimal("sum_qty"),
-        round_decimal("sum_base_price"),
-        round_decimal("sum_disc_price"),
-        round_decimal("sum_charge"),
-        round_decimal("avg_qty"),
-        round_decimal("avg_price"),
-        round_decimal("avg_disc"),
-        col("count_order"),
-    )
+    return (df_selections, expected_schema, expected_selections)
 
-    expected_result = {
-        "l_returnflag": ["A", "N", "N", "R"],
-        "l_linestatus": ["F", "F", "O", "F"],
-        "sum_qty": [117597.0, 2923.0, 220142.0, 114701.0],
-        "sum_base_price": [177256208.31, 4510302.73, 329823621.22, 171489097.87],
-        "sum_disc_price": [168202735.21, 4305696.73, 313366609.14, 162841193.55],
-        "sum_charge": [174926201.01, 4493715.13, 325821079.94, 169372092.98],
-        "avg_qty": [25.43, 23.57, 25.47, 25.2],
-        "avg_price": [38325.67, 36373.41, 38160.78, 37681.63],
-        "avg_disc": [0.05, 0.05, 0.05, 0.05],
-        "count_order": [4625, 124, 8643, 4551],
-    }
+def check_q17(df):
+    raw_value = df.collect()[0]["avg_yearly"][0].as_py()
+    value = round(raw_value, 2)
+    assert abs(value - 348406.05) < 0.001
 
-    cols = list(expected_result.keys())
-    df_expected = q01_pricing_summary_report.ctx.from_pydict(expected_result)
+@pytest.mark.parametrize(
+    ("query_code", "answer_file"),
+    [
+        ("q01_pricing_summary_report", "q1"),
+        ("q02_minimum_cost_supplier", "q2"),
+        ("q03_shipping_priority", "q3"),
+        ("q04_order_priority_checking", "q4"),
+        ("q05_local_supplier_volume", "q5"),
+        ("q06_forecasting_revenue_change", "q6"),
+        ("q07_volume_shipping", "q7"),
+        ("q08_market_share", "q8"),
+        ("q09_product_type_profit_measure", "q9"),
+        ("q10_returned_item_reporting", "q10"),
+        ("q11_important_stock_identification", "q11"),
+        ("q12_ship_mode_order_priority", "q12"),
+        ("q13_customer_distribution", "q13"),
+        ("q14_promotion_effect", "q14"),
+        ("q15_top_supplier", "q15"),
+        ("q16_part_supplier_relationship", "q16"),
+        ("q17_small_quantity_order", "q17"),
+        ("q18_large_volume_customer", "q18"),
+        ("q19_discounted_revenue", "q19"),
+        ("q20_potential_part_promotion", "q20"),
+        ("q21_suppliers_kept_orders_waiting", "q21"),
+        ("q22_global_sales_opportunity", "q22"),
+    ],
+)
+def test_tpch_query_vs_answer_file(query_code: str, answer_file: str):
+    module = import_module(query_code)
+    df = module.df
 
-    assert df.join(df_expected, (cols, cols), "anti").count() == 0
-    assert df.count() == df_expected.count()
+    # Treat q17 as a special case. The answer file does not match the spec. Running at
+    # scale factor 1, we have manually verified this result does match the expected value.
+    if answer_file == "q17":
+        return check_q17(df)
 
+    (df_selections, expected_schema, expected_selections) = selections_and_schema(df.schema())
 
-def test_q02_minimum_cost_supplier():
-    import q02_minimum_cost_supplier
-    df = q02_minimum_cost_supplier.df
+    df = df.select(*df_selections)
 
-    df = df.select(
-        round_decimal("s_acctbal"),
-        col("s_name"),
-        col("n_name"),
-        col("p_partkey"),
-        col("p_mfgr"),
-        col("s_address"),
-        col("s_phone"),
-        col("s_comment"),
-    )
+    read_schema = pa.schema(expected_schema)
 
-    expected_result = {
-        "s_acctbal": [9198.31],
-        "s_name": ["Supplier#000000025"],
-        "n_name": ["RUSSIA"],
-        "p_partkey": [24],
-        "p_mfgr": ["Manufacturer#5"],
-        "s_address": ["RCQKONXMFnrodzz6w7fObFVV6CUm2q"],
-        "s_phone": ["32-431-945-3541"],
-        "s_comment": ["ely regular deposits. carefully regular sauternes engage furiously above the regular accounts. idly "],
-    }
+    df_expected = module.ctx.read_csv(f"../../benchmarks/tpch/data/answers/{answer_file}.out", schema=read_schema, delimiter="|", file_extension=".out")
 
-    cols = list(expected_result.keys())
-    df_expected = q02_minimum_cost_supplier.ctx.from_pydict(expected_result)
+    df_expected = df_expected.select(*expected_selections)
 
-    assert df.join(df_expected, (cols, cols), "anti").count() == 0
-    assert df.count() == df_expected.count()
-
-
-def test_q03_shipping_priority():
-    import q03_shipping_priority
-    df = q03_shipping_priority.df
-
-    df = df.select(
-        col("l_orderkey"),
-        round_decimal("revenue"),
-        col("o_orderdate"),
-        col("o_shippriority")
-    )
-
-    expected_result = {
-        "l_orderkey": [5417],
-        "revenue": [10396.08],
-        "o_orderdate": ["1993-08-24"],
-        "o_shippriority": [0],
-    }
-
-    cols = list(expected_result.keys())
-    df_expected = q03_shipping_priority.ctx.from_pydict(expected_result)
+    cols = list(read_schema.names)
 
     assert df.join(df_expected, (cols, cols), "anti").count() == 0
     assert df.count() == df_expected.count()
 
-test_q03_shipping_priority()
+test_tpch_query_vs_answer_file("q17_small_quantity_order", "q17")
