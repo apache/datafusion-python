85f411318b5581a905911be12f306c04b7474491
Tim Saucer <timsaucer@gmail.com>
2024-12-13 09:45:22 -0500

    Trait definition for plan properties now returns LexOrdering

--- src/dataset_exec.rs
+++ src/dataset_exec.rs
@@ -34,7 +34,7 @@ use datafusion::error::{DataFusionError as InnerDataFusionError, Result as DFRes
 use datafusion::execution::context::TaskContext;
 use datafusion::logical_expr::utils::conjunction;
 use datafusion::logical_expr::Expr;
-use datafusion::physical_expr::{EquivalenceProperties, PhysicalSortExpr};
+use datafusion::physical_expr::{EquivalenceProperties, LexOrdering, PhysicalSortExpr};
 use datafusion::physical_plan::stream::RecordBatchStreamAdapter;
 use datafusion::physical_plan::{
     DisplayAs, DisplayFormatType, ExecutionMode, ExecutionPlan, ExecutionPlanProperties,
@@ -251,7 +251,7 @@ impl ExecutionPlanProperties for DatasetExec {
         self.plan_properties.output_partitioning()
     }
 
-    fn output_ordering(&self) -> Option<&[PhysicalSortExpr]> {
+    fn output_ordering(&self) -> Option<&LexOrdering> {
         None
     }
 
