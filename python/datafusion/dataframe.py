6bba2e2a498a7ed323fa15ac7e4b2d2a90dd9f63
Siew Kam Onn <kosiew@gmail.com>
2025-02-12 13:01:14 +0800

    ruff lint

--- python/datafusion/dataframe.py
+++ python/datafusion/dataframe.py
@@ -124,7 +124,7 @@ class DataFrame:
     def into_view(self) -> pa.Table:
         """Convert DataFrame as a ViewTable which can be used in register_table."""
         return self._into_view()
-    
+
     def _into_view(self) -> pa.Table:
         """Convert DataFrame as a ViewTable which can be used in register_table."""
         return self.df._into_view()
