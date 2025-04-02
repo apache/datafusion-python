eef0a36541fcd83a624d37e606bd62870fb288f9
Siew Kam Onn <kosiew@gmail.com>
2025-04-02 19:20:51 +0800

    debug: Add logging to collect_record_batches_to_display for better traceability

--- src/dataframe.rs
+++ src/dataframe.rs
@@ -904,8 +904,10 @@ async fn collect_record_batches_to_display(
     let mut rows_so_far = 0;
     let mut record_batches = Vec::default();
     let mut has_more = false;
+    println!("==> min_rows: {min_rows}, max_rows: {max_rows}, max_bytes: {max_bytes}");
 
-    while (size_estimate_so_far < max_bytes && rows_so_far < max_rows) || rows_so_far < min_rows {
+    while size_estimate_so_far < max_bytes && rows_so_far < max_rows && rows_so_far < min_rows {
+        println!("==> size_estimate_so_far: {size_estimate_so_far}, rows_so_far: {rows_so_far}");
         let mut rb = match stream.next().await {
             None => {
                 break;
@@ -944,6 +946,9 @@ async fn collect_record_batches_to_display(
             record_batches.push(rb);
         }
     }
+    println!(
+        "==> after while, size_estimate_so_far: {size_estimate_so_far}, rows_so_far: {rows_so_far}"
+    );
 
     if record_batches.is_empty() {
         return Ok((Vec::default(), false));
