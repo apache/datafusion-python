cefbdb0055d0b97c33b9d683f6a26cd42c7c6455
Tim Saucer <timsaucer@gmail.com>
2025-10-12 07:39:40 -0400

    Switching to explicit param_values or named parameters that will perform string replacement via parsed tokens

--- src/sql/util.rs
+++ src/sql/util.rs
@@ -1,93 +1,69 @@
-use datafusion::common::{internal_err, plan_datafusion_err, DataFusionError};
+use datafusion::common::{exec_err, plan_datafusion_err, DataFusionError};
 use datafusion::logical_expr::sqlparser::dialect::dialect_from_str;
 use datafusion::sql::sqlparser::dialect::Dialect;
-use datafusion::sql::sqlparser::keywords::Keyword;
 use datafusion::sql::sqlparser::parser::Parser;
-use datafusion::sql::sqlparser::tokenizer::{Token, Tokenizer, Word};
+use datafusion::sql::sqlparser::tokenizer::{Token, Tokenizer};
 use std::collections::HashMap;
 
-fn value_from_replacements(
+fn tokens_from_replacements(
     placeholder: &str,
-    replacements: &HashMap<String, String>,
-) -> Option<Token> {
+    replacements: &HashMap<String, Vec<Token>>,
+) -> Option<Vec<Token>> {
     if let Some(pattern) = placeholder.strip_prefix("$") {
-        replacements.get(pattern).map(|replacement| {
-            Token::Word(Word {
-                value: replacement.to_owned(),
-                quote_style: None,
-                keyword: Keyword::NoKeyword,
-            })
-        })
+        replacements.get(pattern).cloned()
     } else {
         None
     }
 }
 
-fn table_names_are_valid(dialect: &dyn Dialect, replacements: &HashMap<String, String>) -> bool {
-    for name in replacements.values() {
-        let tokens = Tokenizer::new(dialect, name).tokenize().unwrap();
-        if tokens.len() != 1 {
-            // We should get exactly one token for our temporary table name
-            return false;
-        }
-
-        if let Token::Word(word) = &tokens[0] {
-            // Generated table names should be not quoted or have keywords
-            if word.quote_style.is_some() || word.keyword != Keyword::NoKeyword {
-                return false;
-            }
-        } else {
-            // We should always parse table names to a Word
-            return false;
-        }
-    }
-
-    true
+fn get_tokens_for_string_replacement(
+    dialect: &dyn Dialect,
+    replacements: HashMap<String, String>,
+) -> Result<HashMap<String, Vec<Token>>, DataFusionError> {
+    replacements
+        .into_iter()
+        .map(|(name, value)| {
+            let tokens = Tokenizer::new(dialect, &value)
+                .tokenize()
+                .map_err(|err| DataFusionError::External(err.into()))?;
+            Ok((name, tokens))
+        })
+        .collect()
 }
 
-pub(crate) fn replace_placeholders_with_table_names(
+pub(crate) fn replace_placeholders_with_strings(
     query: &str,
     dialect: &str,
     replacements: HashMap<String, String>,
 ) -> Result<String, DataFusionError> {
-    let dialect = dialect_from_str(dialect).ok_or_else(|| {
-        plan_datafusion_err!(
-            "Unsupported SQL dialect: {dialect}. Available dialects: \
-                     Generic, MySQL, PostgreSQL, Hive, SQLite, Snowflake, Redshift, \
-                     MsSQL, ClickHouse, BigQuery, Ansi, DuckDB, Databricks."
-        )
-    })?;
+    let dialect = dialect_from_str(dialect)
+        .ok_or_else(|| plan_datafusion_err!("Unsupported SQL dialect: {dialect}."))?;
 
-    if !table_names_are_valid(dialect.as_ref(), &replacements) {
-        return internal_err!("Invalid generated table name when replacing placeholders");
-    }
-    let tokens = Tokenizer::new(dialect.as_ref(), query).tokenize().unwrap();
+    let replacements = get_tokens_for_string_replacement(dialect.as_ref(), replacements)?;
+
+    let tokens = Tokenizer::new(dialect.as_ref(), query)
+        .tokenize()
+        .map_err(|err| DataFusionError::External(err.into()))?;
 
     let replaced_tokens = tokens
         .into_iter()
-        .map(|token| {
-            if let Token::Word(word) = &token {
-                let Word {
-                    value,
-                    quote_style: _,
-                    keyword: _,
-                } = word;
-
-                value_from_replacements(value, &replacements).unwrap_or(token)
-            } else if let Token::Placeholder(placeholder) = &token {
-                value_from_replacements(placeholder, &replacements).unwrap_or(token)
+        .flat_map(|token| {
+            if let Token::Placeholder(placeholder) = &token {
+                tokens_from_replacements(placeholder, &replacements).unwrap_or(vec![token])
             } else {
-                token
+                vec![token]
             }
         })
         .collect::<Vec<Token>>();
 
-    Ok(Parser::new(dialect.as_ref())
+    let statement = Parser::new(dialect.as_ref())
         .with_tokens(replaced_tokens)
         .parse_statements()
-        .map_err(|err| DataFusionError::External(Box::new(err)))?
-        .into_iter()
-        .map(|s| s.to_string())
-        .collect::<Vec<_>>()
-        .join(" "))
+        .map_err(|err| DataFusionError::External(Box::new(err)))?;
+
+    if statement.len() != 1 {
+        return exec_err!("placeholder replacement should return exactly one statement");
+    }
+
+    Ok(statement[0].to_string())
 }
