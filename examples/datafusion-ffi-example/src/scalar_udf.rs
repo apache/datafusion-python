a2b5f894f12fe2a86bfaa7ffc8bdc63cbc7d7dcd
Tim Saucer <timsaucer@gmail.com>
2026-02-06 08:07:08 -0500

    Update example to rust2024

--- examples/datafusion-ffi-example/src/scalar_udf.rs
+++ examples/datafusion-ffi-example/src/scalar_udf.rs
@@ -20,15 +20,15 @@ use std::sync::Arc;
 
 use arrow_array::{Array, BooleanArray};
 use arrow_schema::DataType;
-use datafusion_common::error::Result as DataFusionResult;
 use datafusion_common::ScalarValue;
+use datafusion_common::error::Result as DataFusionResult;
 use datafusion_expr::{
     ColumnarValue, ScalarFunctionArgs, ScalarUDF, ScalarUDFImpl, Signature, TypeSignature,
     Volatility,
 };
 use datafusion_ffi::udf::FFI_ScalarUDF;
 use pyo3::types::PyCapsule;
-use pyo3::{pyclass, pymethods, Bound, PyResult, Python};
+use pyo3::{Bound, PyResult, Python, pyclass, pymethods};
 
 #[pyclass(name = "IsNullUDF", module = "datafusion_ffi_example", subclass)]
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
